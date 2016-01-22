7488b4a6dfef0c7d51cce07f0f4edff9fd99bb7b
panox <panospalos@gmail.com>
2016-01-22 00:42:45 +0000

    server port

---
+++ webserver/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Port
+app.listen(3000);
+console.log('listening to 3000');
