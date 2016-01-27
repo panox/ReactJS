21287137d7cdc992228620f5b368da7f58b3db9e
panox <panospalos@gmail.com>
2016-01-27 18:44:09 +0000

    render function

---
+++ products/client/script.jsx
@@ -0,0 +1,11 @@
+var Products = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <h2>Products</h2>
+        <ProductForm />
+        <ProductList />
+      </div>
+    );
+  }
+});
