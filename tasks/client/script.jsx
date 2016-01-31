2792369e1388a321080c7a25d2cb6b5d6e2b7b5c
panox <panospalos@gmail.com>
2016-01-31 19:01:06 +0200

    render the list

--- tasks/client/script.jsx
+++ tasks/client/script.jsx
@@ -1,6 +1,15 @@
 var ReactCSSTransitionGroup = React.addons.ReactCSSTransitionGroup;
 var TaskList = React.createClass({
   getInitialState: function () {
-    
+    return {items: ['bring the kids to school']}
+  },
+  render: function () {
+    var items = this.state.items.map(function (item, index) {
+      return(
+        <div key={item} onClick={this.handleRemove.bind(this, index)}>
+          {item}
+        </div>
+      )
+    })
   }
 });
