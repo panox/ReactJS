730d8c6a7712c715d9c17d1b5c7fcdabb2aa2bea
panox <panospalos@gmail.com>
2016-01-31 19:18:14 +0200

    error fixed

--- tasks/client/script.jsx
+++ tasks/client/script.jsx
@@ -1,38 +1,37 @@
-var ReactCSSTransitionGroup = React.addons.ReactCSSTransitionGroup;
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var TaskList = React.createClass({
-  getInitialState: function () {
-    return {items: ['bring the kids to school']}
+  getInitialState: function(){
+    return {items: ['Bring the kids to school - 7am']}
   },
-  handleAdd: function () {
-    var newItems = this.state.items.concat([prompt('Add a task')]);
-    this.setState({
-      items: newItems
-    });
+  handleAdd: function(){
+    var newItems =
+      this.state.items.concat([prompt('Add A Task')]);
+      this.setState({items: newItems});
   },
-  handleRemove: function () {
+  handleRemove: function(index){
     var newItems = this.state.items;
-    newItems.splice(i, 1);
-    this.setState({
-      items: newItems
-    });
+    newItems.splice(index, 1);
+    this.setState({items: newItems});
   },
-  render: function () {
-    var items = this.state.items.map(function (item, index) {
-      return(
-        <div key={item} onClick={this.handleRemove.bind(this, index)}>
-          {item}
+  render: function(){
+    var items = this.state.items.map(function(item, index){
+      return (
+          <div key={item} onClick={this.handleRemove.bind(this, index)}>
+            {item}
+          </div>
+        );
+    }.bind(this));
+    return(
+        <div>
+          <button className="btn btn-primary btn-block" onClick={this.handleAdd}>Add Task</button>
+          <ReactCSSTransitionGroup transitionName="tasks"
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={500}>
+          {items}
+          </ReactCSSTransitionGroup>
         </div>
       );
-    }.bind(this));
-    return (
-      <div>
-        <button className="btn btn-primary btn-block" onClick={this.handleAdd}>Add Task</button>
-        <ReactCSSTransitionGroup tranistionName="tasks" tranistionTimeout={1000}
-        tranistionLeaveTimeout={500}>
-        {items}
-        </ReactCSSTransitionGroup>
-      </div>
-    );
   }
 });
-ReactDOM.render(<TaskList />, document.getElementById('tasks'));
+
+React.render(<TaskList />, document.getElementById('tasks'));
