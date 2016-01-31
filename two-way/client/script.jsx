2e3b5f536f70324e8e210205397c1210df555761
panox <panospalos@gmail.com>
2016-01-31 19:39:05 +0200

    component render

--- two-way/client/script.jsx
+++ two-way/client/script.jsx
@@ -1,5 +1,18 @@
 var MyComponent = React.createClass({
-  
+  mixins: [Reac.addon.LinkedStateMixin],
+  getInitialState: function () {
+    return {
+      value: 'Some Value'
+    };
+  },
+  render: function() {
+    return (
+      <div>
+        <h4>{this.state.value}</h4>
+        <MyInput />
+      </div>
+    );
+  }
 });
 
 var MyInput = React.createClass({
