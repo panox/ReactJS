c58f8e92f122356be8cc4cfef3c7e46977ac28ec
panox <panospalos@gmail.com>
2016-02-04 16:47:55 +0000

    getDefaultProps for Profile

--- gitProfile/client/script.jsx
+++ gitProfile/client/script.jsx
@@ -1,4 +1,17 @@
 var Profile = React.createClass({
+  getDefaultProps: function () {
+    return {
+      clientId: '3e78b0022afdceee9311',
+      clientSecret: '5180ff5893ac9764b93b9155b46c329e3543e313',
+      urls : {
+        user: 'https://api.github.com/users'
+      },
+      perPage: 5
+    };
+  },
+  getInitialState : function () {
+
+  },
   render: function() {
     return (
       <div>
