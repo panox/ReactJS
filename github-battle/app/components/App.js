e7d6a8477e4c20d6e7ddb6bd55e35a3acf4b7868
panox <panospalos@gmail.com>
2017-08-11 18:13:14 +0100

    export module

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
+var Popular = require('./components/Popular.js');
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello React!
+            <Popular />
         </div>
       )
     }
