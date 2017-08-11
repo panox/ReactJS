3ee3afd7fef25d8865f8083df6dc19eb4e26fa5e
panox <panospalos@gmail.com>
2017-08-11 18:17:34 +0100

    css

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -1,10 +1,10 @@
 var React = require('react');
-var Popular = require('./components/Popular.js');
+var Popular = require('./Popular');
 
 class App extends React.Component {
     render() {
       return (
-        <div>
+        <div className='container'>
             <Popular />
         </div>
       )
